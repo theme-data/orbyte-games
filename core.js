7fea08fc08d8ab76f6da8a5b690b0ded82e421da
monteiroux <pd.data.user@gmail.com>
2026-09-14 18:09:17 -0300

    Ajusta

--- core.js
+++ core.js
@@ -150,7 +150,7 @@ $(document).ready(function(){
           {
               breakpoint: 768,
               settings: {
-                  slidesToShow: 3
+                  slidesToShow: 4
               }
           }
       ]
